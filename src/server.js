8f401ddd56c31e7f461d3cf7a6d2659da7e2ab42
emptyelephants <emptyelephants@gmail.com>
2020-02-13 16:33:41 -0800

    mirage js working

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-// fake server mockup using miragejs
-
 import { Server, Model } from 'miragejs';
+// fake server mockup using miragejs
 
 export function makeServer({ environment = 'development' } = {}) {
   const server = new Server({
@@ -11,8 +10,33 @@ export function makeServer({ environment = 'development' } = {}) {
       user: Model,
     },
     seeds(mock) {
-      mock.create('user', { name: 'Bob' });
-      mock.create('user', { name: 'Alice' });
+      mock.create('user', {
+        name: 'Miguel',
+        budgetExpendatures: [{
+          entryTitle: 'Very Good Building LLC Payroll',
+          cost: 1000,
+          doesReoccur: false,
+          budgetDate: 1578384000000,
+        },
+        {
+          entryTitle: 'Burger',
+          cost: -10,
+          doesReoccur: false,
+          budgetDate: 1578988800000,
+        },
+        {
+          entryTitle: 'Gas',
+          cost: -20,
+          doesReoccur: false,
+          budgetDate: 1579852800000,
+        },
+        {
+          entryTitle: 'Feb Burger',
+          cost: -20,
+          doesReoccur: false,
+          budgetDate: 1580889600000,
+        }],
+      });
     },
     routes() {
       this.namespace = 'api';
